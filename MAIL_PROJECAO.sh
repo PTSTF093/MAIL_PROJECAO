5f5f45d5d8365b2aa15de04ac28125bf3056311f
PTSTF093 <52711789+PTSTF093@users.noreply.github.com>
2019-08-21 14:00:07 +0100

    afinado

--- MAIL_PROJECAO.sh
+++ MAIL_PROJECAO.sh
@@ -15,15 +15,17 @@ Abertura do online:
 user="root"
 password="openpipes"
 
-declare -a CADAMEIAHORA=('00:30:00' '01:00:00' '01:30:00' '02:00:00' '02:30:00' '03:00:00' '03:30:00' '04:00:00' '04:30:00' '05:00:00' '05:30:00' '06:00:00' '06:30:00' '07:00:00' '07:30:00' '08:00:00')
+declare -a CADAMEIAHORA=('00:15:00' '00:30:00' '00:45:00' '01:00:00' '01:15:00' '01:30:00' '01:45:00' '02:00:00' '02:15:00' '02:30:00' '02:45:00' '03:00:00' '03:15:00' '03:30:00' '03:45:00' '04:00:00' '04:15:00' '04:30:00' '04:45:00' '05:00:00' '05:15:00' '05:30:00' '05:45:00' '06:00:00' '06:15:00' '06:30:00' '06:45:00' '07:00:00' '07:15:00' '07:30:00' '07:45:00' '08:00:00')
 
 hoje=$(date +"%Y-%m-%d")
 #enderecos="ptstf093@pt.ibm.com,ptstf031@pt.ibm.com,ptstf026@pt.ibm.com"
-enderecos="ptstf093@pt.ibm.com,jose.gaspar@pt.ibm.com,ptstf026@pt.ibm.com"
+enderecos="ptstf093@pt.ibm.com,jose.gaspar@pt.ibm.com,ptstf026@pt.ibm.com,ptstf031@pt.ibm.com"
 #enderecos="ptstf093@pt.ibm.com,jose.gaspar@pt.ibm.com"
+#enderecos="ptstf093@pt.ibm.com"
 
 #AO=$(echo "SELECT projecao FROM auditoria.projecao order by agora asc limit 1" | mysql auditoria -u $user -p$password)
-AO=$(mysql auditoria -u $user -p$password -se "SELECT projecao FROM auditoria.projecao order by agora desc limit 1"|cut -f1)
+#AO=$(mysql auditoria -u $user -p$password -se "SELECT projecao FROM auditoria.projecao order by agora desc limit 1"|cut -f1)
+AO=$(mysql auditoria -u $user -p$password -se "SELECT hora FROM auditoria.AberturaOnline order by instante desc limit 1"|cut -f1)
 
 #QUAL o JOB do ONLINE de HOJE?
 ONLINE=$(mysql auditoria -u $user -p$password -se "SELECT online FROM auditoria.projecao order by agora desc limit 1"|cut -f1)
@@ -36,26 +38,34 @@ echo > $F
 #Algum conteúdo para o mail
 echo "Bom dia," >> $F
 echo "" >> $F
-echo "segue o controle de estimativa para a projeção do batch de hoje e para o online $ONLINE" >> $F
+echo "segue o controle de eficácia da projeção da hora de execução do SLA $ONLINE, relativo ao batch desta noite." >> $F
 echo "" >> $F
-echo "Legendas:
-HORA REC -> hora da recolha
-PROJECAO -> previsao feita na hora da recolha
-T2P      -> Time to Projecao
-A.ONLINE -> Abertura do Online
-T2AO     -> Time to Abertura do Online (Real)
-DIFEREN. -> Diferança entre T2P e T2AO
-
-
-" >> $F
+#echo "Legendas:
+#HORA REC -> hora da recolha
+#T2P      -> Time to Projecao
+#T2AO     -> Time to Abertura do Online (Real)
+#DIFEREN. -> Diferança entre T2P e T2AO
+#
+#
+#" >> $F
 
 #cabecalho
 #00:30:00 | 02:45:16 | 02:15:16 | 02:13:03 | 01:43:03 | 01:02:13
-echo "HORA REC | PROJECAO | T2P      | A.ONLINE | T2AO     | DIFEREN."
-echo "---------------------------------------------------------------" >> $F
-echo "HORA REC | PROJECAO | T2P      | A.ONLINE | T2AO     | DIFEREN." >> $F
-echo "---------------------------------------------------------------"
-echo "---------------------------------------------------------------" >> $F
+echo "+--------------------+------------+------------+---------+
+|DATA/HORA RECOLHA   |Time to SLA |Time to SLA |Diferença|
+|                    |(estimativa)| (real)     |         |
++--------------------+------------+------------+---------+" >>$F
+
+echo "+--------------------+------------+------------+---------+
+|DATA/HORA RECOLHA   |Time to SLA |Time to SLA |Diferença|
+|                    |(estimativa)| (real)     |         |
++--------------------+------------+------------+---------+"
+
+
+#echo "HORA REC            | T2P      | T2AO     | DIFEREN."
+#echo "----------------------------------------------------" >> $F
+#echo "HORA REC            | T2P      | T2AO     | DIFEREN." >> $F
+#echo "----------------------------------------------------" >> $F
 
 for meia in "${CADAMEIAHORA[@]}" ; do
 	#calcular a diferenca de agora, pe 00:30 para a abertura previsa do online
@@ -78,10 +88,23 @@ for meia in "${CADAMEIAHORA[@]}" ; do
 	
 	#display dos resultados
 	if [ ! -z "$PROJECAO" ] ; then
-		echo "$meia | $PROJECAO | $T2P | $AO | $T2SLA | $DIFERENCA"
-		echo "$meia | $PROJECAO | $T2P | $AO | $T2SLA | $DIFERENCA" >> $F
+		echo "|$hoje $meia | $T2P   | $T2SLA   | $DIFERENCA|" 
+		echo "|$hoje $meia | $T2P   | $T2SLA   | $DIFERENCA|" >> $F
 	fi
 done
 
+#Finalmente imprimir a ultima recolha de informação
+#Ultima hora existente na tabela de projeção
+#UH=$(mysql auditoria -u $user -p$password -se "SELECT agora FROM auditoria.projecao ORDER BY agora DESC LIMIT 1 " |cut -f1)
+#UPROJECAO=$(mysql auditoria -u $user -p$password -se "SELECT projecao FROM auditoria.projecao WHERE agora = '$UH'" |cut -f1)
+#UT2P=$(mysql auditoria -u $user -p$password -se "select SEC_TO_TIME( (TIME_TO_SEC('$UPROJECAO')) - (TIME_TO_SEC('$UH')) )" |cut -f1)
+#echo "$UH | $UT2P | -------- | --------"
+#echo "$UH | $UT2P | -------- | --------" >> $F
+echo "+--------------------+------------+------------+---------+" >> $F
+echo "|$hoje $AO ===> $ONLINE executado!            |" >> $F
+echo "+--------------------+------------+------------+---------+" >> $F
+echo "+--------------------+------------+------------+---------+"
+echo "|$hoje $AO ===> $ONLINE executado!            |"
+echo "+--------------------+------------+------------+---------+"
 #Iterar pelo ficheiro e montar um mail
 cat $F | mutt -s "$ONLINE - controles de estimativa" -- $enderecos
